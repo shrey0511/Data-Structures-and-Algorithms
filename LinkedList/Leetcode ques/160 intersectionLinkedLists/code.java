3109948dd8b97d7513aa97d5324a4bc466289d49
shrey0511 <112866424+shrey0511@users.noreply.github.com>
2024-05-26 10:58:14 +0530

    Added ques 160

--- LinkedList/Leetcode ques/160 intersectionLinkedLists/code.java
+++ LinkedList/Leetcode ques/160 intersectionLinkedLists/code.java
@@ -4,6 +4,25 @@ public class code {
     public static void main(String[] args) {
         
     }
+
+    //* Definition of LinkedList
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
     
-    static 
+    static ListNode getIntersectionNode(ListNode headA , ListNode headB){
+        if(headA == null || headB == null) return null;
+
+        ListNode a = headA , b = headB;
+        while(a != b){
+            a = (a==null) ? b : a.next;
+            b = (b==null) ? a : b.next;
+        }
+        return a;
+    }
 }
