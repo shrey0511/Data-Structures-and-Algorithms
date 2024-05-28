89b47982c89cc363b07e9a8aa512d0643fbc16b6
shrey0511 <112866424+shrey0511@users.noreply.github.com>
2024-05-28 12:17:48 +0530

    Ques 1208M added

---
+++ DailyChallenge/1208M equalSubstringsWithinBudget/code.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/get-equal-substrings-within-budget/description/?envType=daily-question&envId=2024-05-28
+
+public class code {
+    public static void main(String[] args) {
+        
+    }
+
+    static int equalSubstring(String s, String t, int maxCost) {
+        int[] diff = new int[s.length()];
+        for(int i=0 ; i<s.length() ; i++){
+            int asciiS = s.charAt(i);
+            int asciiT = t.charAt(i);
+            diff[i] = Math.abs(asciiS - asciiT);
+        }
+        int maxLen = 0 , currCost = 0 , start = 0;
+        for(int end = 0 ; end<diff.length ; end++){
+            currCost += diff[end];
+            while(currCost > maxCost){
+                currCost -= diff[start];
+                ++start;
+            }
+            maxLen = Math.max(maxLen, end-start+1);
+        }
+        return maxLen;
+    }
+}
