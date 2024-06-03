531daf0469b93c65900bd6aa73d94c0fdafa2b43
shrey0511 <112866424+shrey0511@users.noreply.github.com>
2024-06-03 18:48:41 +0530

    More LinkedList ques

--- LinkedList/Leetcode ques/138M copylistWithRandomPointer/code.java
+++ LinkedList/Leetcode ques/138M copylistWithRandomPointer/code.java
@@ -1,6 +1,10 @@
 //https://leetcode.com/problems/copy-list-with-random-pointer/
 
 // Definition for a Node.
+
+import java.util.HashMap;
+import java.util.Map;
+
 class Node {
     int val;
     Node next;
@@ -18,5 +22,24 @@ public class code {
         
     }
 
-    
+    static Node copyRandomList(Node head){
+        if(head == null) return null;
+
+        Map<Node,Node> map = new HashMap<>();
+        
+        Node curr = head;
+        while(curr != null){
+            map.put(curr , new Node(curr.val));
+            curr = curr.next;
+        }
+
+        curr = head;
+        while(curr != null){
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
+        }
+
+        return map.get(head);
+    }
 }
---
+++ LinkedList/Leetcode ques/82M removeDuplicatesFromSortedListII/code.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+
+
+public class code {
+    public static void main(String[] args) {
+        
+    }
+
+    //* Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    
+}
