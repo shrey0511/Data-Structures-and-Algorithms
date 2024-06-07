9d372319e725cac9019e296711e60f48f1b38506
shrey0511 <112866424+shrey0511@users.noreply.github.com>
2024-06-07 13:15:56 +0530

    More on Linked List

--- LinkedList/Leetcode ques/82M removeDuplicatesFromSortedListII/code.java
+++ LinkedList/Leetcode ques/82M removeDuplicatesFromSortedListII/code.java
@@ -1,5 +1,5 @@
 //https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
-
+//Asked in Amex
 
 public class code {
     public static void main(String[] args) {
@@ -7,7 +7,7 @@ public class code {
     }
 
     //* Definition for singly-linked list.
-    public class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
@@ -15,5 +15,23 @@ public class code {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
+    static ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = head , prev = dummyHead;
+        while(temp!= null && temp.next != null){
+            if(temp.val == temp.next.val){
+                int val = temp.val;
+                while(temp!= null && temp.val == val)
+                    temp = temp.next;
+                prev.next = temp;
+            }else{
+                prev = temp;
+                temp = temp.next;
+            }
+        }
+        return dummyHead.next;
+    }
     
 }
---
+++ LinkedList/Leetcode ques/86M partitionList/code.java
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/partition-list/
+
+public class code {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
