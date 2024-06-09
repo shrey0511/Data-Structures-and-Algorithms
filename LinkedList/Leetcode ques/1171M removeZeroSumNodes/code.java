87a822a5db91360399198fed9ff4188b6747c3e4
shrey0511 <112866424+shrey0511@users.noreply.github.com>
2024-06-09 21:14:33 +0530

    ques 1171M added

--- LinkedList/Leetcode ques/1171M removeZeroSumNodes/code.java
+++ LinkedList/Leetcode ques/1171M removeZeroSumNodes/code.java
@@ -14,5 +14,33 @@ public class code {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
-    static 
+    static ListNode removeZeroSumSublists(ListNode head) {
+        if(head.val == 0 && head.next == null) return null;
+        if(head.next == null) return head;
+        //Dummy node to handle edge cases of head removal
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        HashMap<Integer,ListNode> map = new HashMap<>();
+        int prefixSum = 0;
+
+        for(ListNode curr = dummy ; curr!=null ; curr=curr.next){
+            prefixSum += curr.val;
+
+            if(map.containsKey(prefixSum)){
+                ListNode prev = map.get(prefixSum);
+                ListNode toRemove = prev.next;
+                int p = prefixSum + (toRemove!=null? toRemove.val : 0) ;
+                while(p != prefixSum){
+                    map.remove(p);
+                    toRemove = toRemove.next;
+                    p += (toRemove != null)? toRemove.val : 0;
+                }
+                prev.next = curr.next;
+            }else{
+                map.put(prefixSum,curr);
+            }
+        }
+        return dummy.next;
+    }
 }
